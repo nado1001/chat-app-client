5c85129252927fba3d6321811b2ed40b676d8c77
take-tech1001 <tk.tech1001@gmail.com>
2021-09-08 02:04:03 +0900

    Move pages & styles to src folder.

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>チャットルームリスト</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <div>
+          チャットルームリスト
+        </div>
+      </main>
+
+    </div>
+  )
+}
+
+export default Home
